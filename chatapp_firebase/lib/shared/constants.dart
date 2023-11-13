6388bb3ad6d0b64e2cb5f9f136298129560e12d9
Geza <topdevspain@gmail.com>
2023-11-13 13:31:39 +0100

    first commit

---
+++ chatapp_firebase/lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:298000387127:web:90c01de8d176475cf783b2";
+  static String apiKey = "AIzaSyB0774Bb6SLi8Oj8U2WE2-rR5qldwf8vH4";
+  static String messagingSenderId = "298000387127";
+  static String projectId = "chatappflutter-4ef06";
+  final primaryColor = const Color(0xFFee7b64);
+}
